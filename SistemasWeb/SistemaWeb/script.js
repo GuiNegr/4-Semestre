fabd90fbe0e584051724ab05123a3958fc9cae5e
GuiNegr <GuiNegr.git@outlook.com>
2024-08-23 14:31:05 -0300

    upando a aula do dia 22

---
+++ SistemasWeb/SistemaWeb/script.js
@@ -0,0 +1,17 @@
+const h1 = document.querySelector("h1");
+document.addEventListener('DOMContentLoaded', () => {
+    h1.innerHTML = "Adeus"
+}
+)
+
+document.addEventListener('click' , () =>{
+    h1.innerHTML = "Clicando"
+})
+
+document.addEventListener('dblclick', () => {
+        h1.innerHTML = "cricou duas vzs"
+        const entrada = window.prompt("Digite seu nome")
+        h1.innerHTML = entrada
+
+        
+    })
